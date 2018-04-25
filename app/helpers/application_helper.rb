b2b97d340947fb082007ae07707c9ee0bfeebcc7
Gerard Sargent <gedsargent@icloud.com>
2018-04-25 15:03:23 +0100

    Fixed devise inauthenticity token on signout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def login_helper
     if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path(:authenticity_token => form_authenticity_token()), method: 'delete'
     else
       (link_to "Register", new_user_registration_path) + 
       "<br>".html_safe + 
